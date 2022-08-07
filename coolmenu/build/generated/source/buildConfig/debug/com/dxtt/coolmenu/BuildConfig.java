60ebbf6f4cc3eaba9b73411ec0ccf2bbb3d6de02
3mr3llam <99687598+3mr3llam@users.noreply.github.com>
2022-08-07 15:58:04 +0200

    First commit

---
+++ coolmenu/build/generated/source/buildConfig/debug/com/dxtt/coolmenu/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dxtt.coolmenu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dxtt.coolmenu";
+  public static final String BUILD_TYPE = "debug";
+}
